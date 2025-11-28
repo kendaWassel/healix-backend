23f6f3c78dff063a6e78cf42d9e0dafc474f3663
hilal-sudo <hilalhhashem13@gmail.com>
2025-11-28 23:34:38 +0300

    update

--- app/Models/careProvider.php
+++ app/Models/careProvider.php
@@ -4,29 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Models\User;          
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Appointment;   
 
 class CareProvider extends Model
 {
-    use HasFactory;
     protected $fillable = [
         'user_id',
         'care_provider_image_id',
         'license_file_id',
         'session_fee',
         'bank_account',
-        'type',
+        'type', 
         'rating_avg'
     ];
-    protected $table='care_providers';
 
     public function user()
     {
         return $this->belongsTo(User::class);
     }
-    public function homeVisits()
-    {
-        return $this->hasMany(HomeVisit::class);
-    }   
 
+    public function appointments()
+    {
+        return $this->hasMany(Appointment::class, 'care_provider_id');
+    }
 }
